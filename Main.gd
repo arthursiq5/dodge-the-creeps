456c1bc35ec1c155fd64c10022f2e21d8169caf8
arthursiq5 <arthur.siq5@gmail.com>
2022-04-03 19:21:26 -0300

    Criada cena principal

---
+++ Main.gd
@@ -0,0 +1,47 @@
+extends Node
+
+export (PackedScene) var mob_scene
+var score
+
+func _ready():
+	randomize()
+	new_game()
+
+
+
+func game_over():
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+
+func new_game():
+	score = 0
+	$Player.start($StartPosition.position)
+	$StartTimer.start()
+
+
+func _on_StartTimer_timeout():
+	$MobTimer.start()
+	$ScoreTimer.start()
+
+func _on_ScoreTimer_timeout():
+	score += 1
+
+
+func _on_MobTimer_timeout():
+	var mob = mob_scene.instance()
+	var mob_spawn_location = get_node("MobPath/MobSpawnLocation")
+	
+	# Seleciona uma direção perpendicular ao caminho para o mob
+	var direction = mob_spawn_location.rotation + PI / 2
+	
+	# define a posição do mob para um local aleatório
+	mob.position = mob_spawn_location.position
+	
+	# adiciona detalhes randômicos à direção
+	direction += rand_range(-PI / 4, PI / 4)
+	mob.rotation = direction
+	
+	# define a velocidade do mob
+	var velocity = Vector2(rand_range(150.0, 250.0), 0.0)
+	
+	add_child(mob)
