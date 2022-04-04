d006619ccbdeeee11708c3145aee24a418cce0a5
arthursiq5 <arthur.siq5@gmail.com>
2022-04-03 21:27:34 -0300

    Iniciadas correções do sistema para o funcionamento correto

--- HUD.gd
+++ HUD.gd
@@ -2,7 +2,6 @@ extends CanvasLayer
 
 signal start_game
 
-
 func show_message(text):
 	$Message.text = text
 	$Message.show()
@@ -12,17 +11,16 @@ func show_game_over():
 	show_message("Game Over")
 	# espera até o MessageTimer terminar de contar
 	yield($MessageTimer, "timeout")
-
+	
 	$Message.text = "Dodge the\nCreeps!"
 	$Message.show()
+	
 	# cria um timer de 1 segundo e espera ele terminar
 	yield(get_tree().create_timer(1), "timeout")
 	$StartButton.show()
 
 func update_score(score):
 	$ScoreLabel.text = str(score)
-
-
 
 func _on_MessageTimer_timeout():
 	$StartButton.hide()
--- Player.gd
+++ Player.gd
@@ -14,23 +14,21 @@ func start(pos):
 	$CollisionShape2D.disabled = false
 
 func _process(delta):
-	var velocity = Vector2.ZERO # vetor de velocidade do player
-	
-	if Input.is_action_pressed("move_right"):
+	var velocity = Vector2()  # The player's movement vector.
+	if Input.is_action_pressed("ui_right"):
 		velocity.x += 1
-	if Input.is_action_pressed("move_left"):
+	if Input.is_action_pressed("ui_left"):
 		velocity.x -= 1
-	if Input.is_action_pressed("move_down"):
+	if Input.is_action_pressed("ui_down"):
 		velocity.y += 1
-	if Input.is_action_pressed("move_up"):
+	if Input.is_action_pressed("ui_up"):
 		velocity.y -= 1
-	
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 		$AnimatedSprite.play()
 	else:
 		$AnimatedSprite.stop()
-	
+		
 	position += velocity * delta
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
@@ -38,13 +36,14 @@ func _process(delta):
 	if velocity.x != 0:
 		$AnimatedSprite.animation = "walk"
 		$AnimatedSprite.flip_v = false
+		# See the note below about boolean assignment
 		$AnimatedSprite.flip_h = velocity.x < 0
 	elif velocity.y != 0:
 		$AnimatedSprite.animation = "up"
 		$AnimatedSprite.flip_v = velocity.y > 0
 
 func _on_Player_body_entered(body):
-	hide() # Player disappears after being hit.
+	hide()  # Player disappears after being hit.
 	emit_signal("hit")
-	# Must be deferred as we can't change physics properties on a physics callback.
 	$CollisionShape2D.set_deferred("disabled", true)
+
