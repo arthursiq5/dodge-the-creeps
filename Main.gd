4ee3d0a0cf7d0dd025f7d1505c8fd810c0e28748
arthursiq5 <arthur.siq5@gmail.com>
2022-04-03 21:41:50 -0300

    Corrigido jogo e adicionadas músicas

--- Main.gd
+++ Main.gd
@@ -1,18 +1,21 @@
 extends Node
 
-export (PackedScene) var mob_scene
+export (PackedScene) var Mob
 var score
+
 
 func _ready():
 	randomize()
+
 
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
 	$HUD.show_game_over()
 	get_tree().call_group("mobs", "queue_free")
-	#$Music.stop()
-	#$DeathSound.play()
+	$Music.stop()
+	$DeathSound.play()
+
 
 func new_game():
 	score = 0
@@ -20,36 +23,32 @@ func new_game():
 	$StartTimer.start()
 	$HUD.update_score(score)
 	$HUD.show_message("Get Ready")
-	#$Music.play()
+	$Music.play()
 
 
-func _on_StartTimer_timeout():
-	$MobTimer.start()
-	$ScoreTimer.start()
+func _on_MobTimer_timeout():
+	# Choose a random location on Path2D.
+	$MobPath/MobSpawnLocation.offset = randi()
+	# Create a Mob instance and add it to the scene.
+	var mob = Mob.instance()
+	add_child(mob)
+	# Set the mob's direction perpendicular to the path direction.
+	var direction = $MobPath/MobSpawnLocation.rotation + PI / 2
+	# Set the mob's position to a random location.
+	mob.position = $MobPath/MobSpawnLocation.position
+	# Add some randomness to the direction.
+	direction += rand_range(-PI / 4, PI / 4)
+	mob.rotation = direction
+	# Set the velocity (speed & direction).
+	mob.linear_velocity = Vector2(rand_range(mob.min_speed, mob.max_speed), 0)
+	mob.linear_velocity = mob.linear_velocity.rotated(direction)
+
 
 func _on_ScoreTimer_timeout():
 	score += 1
 	$HUD.update_score(score)
 
-func _on_MobTimer_timeout():
-	var mob = mob_scene.instance()
-	var mob_spawn_location = get_node("MobPath/MobSpawnLocation")
-	
-	# escolhe um local aleatório para o Path2D
-	$MobPath/MobSpawnLocation.offset = randi()
-	
-	# Seleciona uma direção perpendicular ao caminho para o mob
-	var direction = mob_spawn_location.rotation + PI / 2
-	
-	# define a posição do mob para um local aleatório
-	mob.position = mob_spawn_location.position
-	
-	# adiciona detalhes randômicos à direção
-	direction += rand_range(-PI / 4, PI / 4)
-	mob.rotation = direction
-	
-	# define a velocidade do mob
-	var velocity = Vector2(rand_range(150.0, 250.0), 0.0)
-	mob.linear_velocity = velocity.rotated(direction)
-	
-	add_child(mob)
+
+func _on_StartTimer_timeout():
+	$MobTimer.start()
+	$ScoreTimer.start()
--- Player.gd
+++ Player.gd
@@ -1,17 +1,14 @@
 extends Area2D
+
 signal hit
 
-export var speed = 400 # velocidade (pixels/sec)
-var screen_size # tamanho da tela
+export var speed = 400
+var screen_size
 
 func _ready():
 	screen_size = get_viewport_rect().size
 	hide()
 
-func start(pos):
-	position = pos
-	show()
-	$CollisionShape2D.disabled = false
 
 func _process(delta):
 	var velocity = Vector2()  # The player's movement vector.
@@ -42,8 +39,14 @@ func _process(delta):
 		$AnimatedSprite.animation = "up"
 		$AnimatedSprite.flip_v = velocity.y > 0
 
+
 func _on_Player_body_entered(body):
 	hide()  # Player disappears after being hit.
 	emit_signal("hit")
 	$CollisionShape2D.set_deferred("disabled", true)
 
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
